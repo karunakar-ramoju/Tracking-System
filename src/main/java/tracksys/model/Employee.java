0866e2ff25a2172381c197dc956bcb4ac7f4fb2a
karunakar-ramoju <karunakar.ramoju@gmail.com>
2017-06-12 08:13:16 +0530

    11_6 Task changes

--- src/main/java/tracksys/model/Employee.java
+++ src/main/java/tracksys/model/Employee.java
@@ -23,6 +23,61 @@ public class Employee {
 	@NotNull
 	@Column(name = "emp_name", nullable = false)	
 	private String name;
+	
+	@Column(name = "emp_address", nullable = false)
+	private String address;
+	
+	@Column(name = "emp_email", nullable = false)
+	private String email;
+	
+	@Column(name = "emp_phone1", nullable = false)
+	private String phone1;
+	
+	@Column(name = "emp_phone2", nullable = false)
+	private String phone2;
+	
+	@Column(name = "emp_refer", nullable = false)
+	private String refer;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone1() {
+		return phone1;
+	}
+
+	public void setPhone1(String phone1) {
+		this.phone1 = phone1;
+	}
+
+	public String getPhone2() {
+		return phone2;
+	}
+
+	public void setPhone2(String phone2) {
+		this.phone2 = phone2;
+	}
+
+	public String getRefer() {
+		return refer;
+	}
+
+	public void setRefer(String refer) {
+		this.refer = refer;
+	}
 
 	public int getId() {
 		return id;
